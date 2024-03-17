7aebe3e26fab067434a64df87c166c651386dd41
hkorets <hko@email.com>
2024-03-17 14:14:12 +0200

    fixed regexes

--- website/auth.py
+++ website/auth.py
@@ -57,7 +57,7 @@ def sign_up():
             flash('Password is too short', category='error')
         elif total_hours > 60:
             flash(f'{total_hours} is to many', category='error')
-        elif bool(re.match('^[А-Я]{3}/\d{2}[А-Я]$', faculty)) is False:
+        elif bool(re.match('^[А-Я]{3}\d{2}/[А-Я]$', faculty)) is False:
             flash('There is no such faculty')
         else:
 
