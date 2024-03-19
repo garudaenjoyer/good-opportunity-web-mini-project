cfba0351864f575d7ed2ca5df7473a7f4387256d
hkorets <hko@email.com>
2024-03-19 23:27:49 +0200

    added regex for add opportunity form

--- website/opportunity_maker.py
+++ website/opportunity_maker.py
@@ -3,6 +3,7 @@ from . import db
 from .models import User, Opportunity
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+import re
 
 opportunity_maker = Blueprint('opportunity_maker', __name__)
 
@@ -21,6 +22,18 @@ def add_opportunity():
             hours = request.form['hours']
             if date and activity and description and location and time\
 and email and phone and hours:
+                if bool(re.match('^[a-zA-Z0-9._%+-]+@ucu\.edu\.ua$', email)) is False:
+                    flash('Email shoud use "ucu.edu.ua" domain', category='error')
+                if bool(re.match('^\+\d{1,3}\s?\(\d{1,3}\)\s?\d{3}-\d{2}-\d{2}$|^\+\d{9,12}$', phone)) is False:
+                    flash('Incorrect phone number format', category='error')
+                if int(hours) > 60:
+                    flash('Hours should not exceed the value of 60', category='error')
+                if int(hours) <= 0:
+                    flash('Hours should be greater than zero', category='error')
+                if len(description) > 1500:
+                    flash('Too many symbols in description', category='error')
+                if len(activity) > 150:
+                    flash('Name of activity can not contain more than 150 symbols', category= 'error')
                 opportunity = Opportunity(date=date, activity=activity, description=description,
                                         location=location, time=time, email=email, phone=phone, hours=hours)
                 db.session.add(opportunity)
